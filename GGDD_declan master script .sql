67eb3e3fd3bd598539d1dc947e7e18b13a6344c6
Darcie McCaughern <darciemccaughern@c02g70vxml85.kainos.com>
2021-10-20 11:09:37 +0100

    sql insert employees

--- GGDD_declan master script .sql
+++ GGDD_declan master script .sql
@@ -15,6 +15,20 @@ employeeId SMALLINT UNSIGNED NOT NULL AUTO_INCREMENT,
     nin char(9) NOT NULL,
     primary key (employeeId)
     );
+
+--Already done
+INSERT INTO Employee(fName, lName, address, city, county, postalCode, accNumber, sortCode, startSalary, nin) 
+VALUES ("Darcie", "Grace", "32 Woodburn Road", "Belfast", "Antrim", "BT6 8GH", 817216254617, 098675, 15600, "PK091789G");
+
+--New Employees
+INSERT INTO Employee(fName, lName, address, city, county, postalCode, accNumber, sortCode, startSalary, nin) 
+VALUES ("Gillon", "Brown", "02 Pearl Street", "Belfast", "Antrim", "BT9 8FG", 891782678526, 018926, 15600, "PJ017825T");
+
+INSERT INTO Employee(fName, lName, address, city, county, postalCode, accNumber, sortCode, startSalary, nin) 
+VALUES ("Gareth", "Moore", "67 Blossom Avenue", "Belfast", "Antrim", "BT8 7FY", 819278165467, 017825, 15600, "PK0018925I");
+
+INSERT INTO Employee(fName, lName, address, city, county, postalCode, accNumber, sortCode, startSalary, nin) 
+VALUES ("Declan", "White", "89 Falls Road", "Belfast", "Antrim", "BT9 9JH", 819267187654, 817276, 15600, "PL817265P");
     
     
      
